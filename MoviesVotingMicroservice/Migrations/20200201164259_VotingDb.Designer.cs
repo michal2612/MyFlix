8023f5e4c81d1d1190fb93fb6c9d3897e8689a09
Michal Domanski <retrospektywny@gmail.com>
2020-02-01 16:45:20 +0000

    voting MS2

--- MoviesVotingMicroservice/Migrations/20200201164259_VotingDb.Designer.cs
+++ MoviesVotingMicroservice/Migrations/20200201164259_VotingDb.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MoviesVotingMicroservice.Models;
 
 namespace MoviesVotingMicroservice.Migrations
 {
     [DbContext(typeof(MovieOpinionContext))]
-    partial class MovieOpinionContextModelSnapshot : ModelSnapshot
+    [Migration("20200201164259_VotingDb")]
+    partial class VotingDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MoviesVotingMicroservice/Migrations/20200201164259_VotingDb.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoviesVotingMicroservice.Migrations
+{
+    public partial class VotingDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_MovieOpinions",
+                table: "MovieOpinions");
+
+            migrationBuilder.RenameTable(
+                name: "MovieOpinions",
+                newName: "MovieOpinion");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MovieOpinion",
+                table: "MovieOpinion",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_MovieOpinion",
+                table: "MovieOpinion");
+
+            migrationBuilder.RenameTable(
+                name: "MovieOpinion",
+                newName: "MovieOpinions");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MovieOpinions",
+                table: "MovieOpinions",
+                column: "Id");
+        }
+    }
+}
