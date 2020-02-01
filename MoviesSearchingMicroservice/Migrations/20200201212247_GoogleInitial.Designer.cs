4c87c2c63876d592370e9efef149d1c431cadf60
Michal Domanski <retrospektywny@gmail.com>
2020-02-01 21:25:36 +0000

    search_3

--- MoviesSearchingMicroservice/Migrations/20200201212247_GoogleInitial.Designer.cs
+++ MoviesSearchingMicroservice/Migrations/20200201212247_GoogleInitial.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MoviesSearchingMicroservice.Models;
 
 namespace MoviesSearchingMicroservice.Migrations
 {
     [DbContext(typeof(SearchContext))]
-    partial class SearchContextModelSnapshot : ModelSnapshot
+    [Migration("20200201212247_GoogleInitial")]
+    partial class GoogleInitial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MoviesSearchingMicroservice/Migrations/20200201212247_GoogleInitial.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoviesSearchingMicroservice.Migrations
+{
+    public partial class GoogleInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SearchMovies",
+                table: "SearchMovies");
+
+            migrationBuilder.RenameTable(
+                name: "SearchMovies",
+                newName: "SearchMovie");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SearchMovie",
+                table: "SearchMovie",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SearchMovie",
+                table: "SearchMovie");
+
+            migrationBuilder.RenameTable(
+                name: "SearchMovie",
+                newName: "SearchMovies");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SearchMovies",
+                table: "SearchMovies",
+                column: "Id");
+        }
+    }
+}
