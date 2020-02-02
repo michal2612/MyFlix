6b8947e50794da7e13d66d9977bef4b70e6dd42a
Michal Domanski <retrospektywny@gmail.com>
2020-02-02 15:42:30 +0000

    webb updated

--- Webb/Models/ClassAPI.cs
+++ Webb/Models/ClassAPI.cs
@@ -8,14 +8,14 @@ namespace Webb.Models
     public static class ClassAPI
     {
         //PATHS
-        private static readonly string _loginPath = "http://192.168.99.100:7000/users-login";
-        private static readonly string _registerPath = "http://192.168.99.100:7000/users-register/";
-        private static readonly string _billingPath = "http://192.168.99.100:7000/billing-checkuser/";
-        private static readonly string _billingCardsPath = "http://192.168.99.100:7000/billing/";
-        private static readonly string _moviesPath = "http://192.168.99.100:7000/movies/";
-        private static readonly string _genresPath = "http://192.168.99.100:7000/genres/";
-        private static readonly string _moviesVotingPath = "http://192.168.99.100:7000/voting/";
-        private static readonly string _votingPath = "http://192.168.99.100:7000/voting-movies/";
+        private static readonly string _loginPath =        "http://35.202.2.227:7001/api/login/";
+        private static readonly string _registerPath =     "http://35.202.2.227:7001/api/register/";
+        private static readonly string _billingPath =      "http://35.202.2.227:7004/api/checkuser/";
+        private static readonly string _billingCardsPath = "http://35.202.2.227:7004/api/billing/";
+        private static readonly string _moviesPath =       "http://35.202.2.227:7003/api/movies/";
+        private static readonly string _genresPath =       "http://35.202.2.227:7003/api/genres/";
+        private static readonly string _moviesVotingPath = "http://35.202.2.227:7002/api/movies/";
+        private static readonly string _votingPath =       "http://35.202.2.227:7002/api/voting/";
 
         //WEB CLIENT
         private static WebClient GetWebclient()
@@ -30,7 +30,7 @@ namespace Webb.Models
         {
             var client = new WebClient();
             client.Headers[HttpRequestHeader.ContentType] = "application/json";
-            return client.UploadString("http://192.168.99.100:7001/api/login", JsonConvert.SerializeObject(new User() { Username = user.Username, Password = user.Password }));
+            return client.UploadString(_loginPath, JsonConvert.SerializeObject(new User() { Username = user.Username, Password = user.Password }));
         }
 
         public static string RegisterUser(User user)
@@ -41,7 +41,7 @@ namespace Webb.Models
         //BILLING
         public static string CheckUser(int id)
         {
-            return GetWebclient().DownloadString("http://192.168.99.100:7004/api/checkuser/" + id.ToString());
+            return GetWebclient().DownloadString(_billingPath + id.ToString());
         }
 
         public static UserCreditCards UserCreditCards(string token)
@@ -58,30 +58,30 @@ namespace Webb.Models
         //MOVIES
         public static MoviesViewModel MovieViewModel()
         {
-            var movies = JsonConvert.DeserializeObject<MovieDto[]>(GetWebclient().DownloadString("http://192.168.99.100:7003/api/movies"));
-            var genres = JsonConvert.DeserializeObject<GenreDto[]>(GetWebclient().DownloadString("http://192.168.99.100:7003/api/genres"));
+            var movies = JsonConvert.DeserializeObject<MovieDto[]>(GetWebclient().DownloadString(_moviesPath));
+            var genres = JsonConvert.DeserializeObject<GenreDto[]>(GetWebclient().DownloadString(_genresPath));
             return new MoviesViewModel() { Movies = movies, Genres = genres };
         }
 
         public static MovieDto[] ReturnMovies()
         {
-            return JsonConvert.DeserializeObject<MovieDto[]>(GetWebclient().DownloadString("http://192.168.99.100:7003/api/movies"));
+            return JsonConvert.DeserializeObject<MovieDto[]>(GetWebclient().DownloadString(_moviesPath));
         }
 
         //MOVIES VOTING
         public static MovieDto[] ReturnVoting(int movieId)
         {
-            return JsonConvert.DeserializeObject<MovieDto[]>(GetWebclient().DownloadString("http://192.168.99.100:7002/api/movies/" + movieId));
+            return JsonConvert.DeserializeObject<MovieDto[]>(GetWebclient().DownloadString(_moviesVotingPath + movieId));
         }
 
         public static MovieDto[] ReturnMoviesVoting(int movieId)
         {
-            return JsonConvert.DeserializeObject<MovieDto[]>(GetWebclient().DownloadString("http://192.168.99.100:7002/api/movies/" + movieId.ToString()));
+            return JsonConvert.DeserializeObject<MovieDto[]>(GetWebclient().DownloadString(_moviesVotingPath + movieId.ToString()));
         }
 
         public static void Vote(VotedViewModel votedViewModel)
         {
-            GetWebclient().UploadString("http://192.168.99.100:7002/api/voting/", JsonConvert.SerializeObject(votedViewModel));
+            GetWebclient().UploadString(_votingPath, JsonConvert.SerializeObject(votedViewModel));
         }
     }
 }
