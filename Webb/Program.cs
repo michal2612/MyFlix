4a8ebc9835c4e7a25733078fc654a79a8d5fc31a
Michal Domanski <retrospektywny@gmail.com>
2020-02-04 21:05:30 +0000

    updated Program.cs

--- Webb/Program.cs
+++ Webb/Program.cs
@@ -12,6 +12,7 @@ namespace Webb
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://*:3000/");
     }
 }
