77c17467e17a62f81e8a26de048b71bf2f2add53
michal2612 <retrospektywny@gmail.com>
2020-06-19 20:47:48 +0100

    Add unit tests

---
+++ NUnitTestProject/BillingTests.cs
@@ -0,0 +1,37 @@
+﻿using BillingMS.Controllers;
+using BillingMS.Models;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTestProject
+{
+    [TestFixture]
+    public class BillingTests
+    {
+        public CreditCard CreditCard { get; set; }
+        public Mock<BillingController> BillingController { get; set; }
+        public Mock<CreditCardContext> CreditCardContext { get; set; }
+
+        public void Setup()
+        {
+            CreditCard = new CreditCard() { Id = 1 };
+        }
+
+        [Test]
+        public void check_the_list_of_credit_cards()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
